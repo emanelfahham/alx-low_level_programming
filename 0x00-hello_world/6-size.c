56afeefa9d24fd3da1362d4f117d16cf60a021b8
emanelfahham <emanelfahham@gmail.com>
2023-06-15 11:59:36 -0700

    adding n

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	float afloat;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-	printf("Size of a int: %lu byte(s)\n", sizeof(ainteger));
+	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
 	printf("Size of a long: %lu byte(s)\n", sizeof(along));
 	printf("Size of a longlong: %lu byte(s)\n", sizeof(alonglong));
 	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
