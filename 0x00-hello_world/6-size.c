02c27646ccae1d728cf4224c8dbc1c9139b364ba
emanelfahham <emanelfahham@gmail.com>
2023-06-15 12:03:02 -0700

    git add

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
 	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
 	printf("Size of a long: %lu byte(s)\n", sizeof(along));
-	printf("Size of a longlong: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of a longlong int: %lu byte(s)\n", sizeof(alonglong));
 	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 	return (0);
 }
