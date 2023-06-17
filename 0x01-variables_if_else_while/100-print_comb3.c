9a852bf3199fcceb8b7d23492df5ce3af39dea0b
emanelfahham <emanelfahham@gmail.com>
2023-06-17 09:25:11 -0700

    fixed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,4 @@
-/**
+/*
  * File: 100-print_comb3.c
  * Auth: Brennan D Baraban
  */
@@ -9,7 +9,7 @@
  * main - Prints all possible combinations of two different digits,
  *        in ascending order, separated by a comma followed by a space.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
